dd2eaaf45b46cbeb2dc8ba8874598921bddc8369
Shreya Mahajan <shreya.mahajan@nitorinfotech.net>
2018-07-09 12:01:18 +0530

    Commit for accepting password in mysql command line for linux

--- pkg/files-posix/plugins/scripts/monitor-mysql-replication-status/replication_status.sh
+++ pkg/files-posix/plugins/scripts/monitor-mysql-replication-status/replication_status.sh
@@ -7,7 +7,17 @@ PORT=$UPTIME_PORT
 
 UPTIMEDIR=`grep pidfile /etc/init.d/uptime_core | head -n 1 | cut -d: -f2 |  cut -c 2- | rev | cut -c 12- | rev`
 
-$UPTIMEDIR/mysql/bin/mysql -u$USER -p$PASSWORD -h$HOSTNAME -P$PORT --protocol=tcp -e "SHOW SLAVE STATUS\G" > test.txt
+TMPFILE=`/bin/mktemp /tmp/uptimeTempConfig.XXXXXXXXXX`
+
+echo "[client]
+user = $UPTIME_USERNAME
+password = $UPTIME_PASSWORD
+host = $UPTIME_HOSTNAME
+port = $UPTIME_PORT" >> "$TMPFILE"
+
+
+
+$UPTIMEDIR/mysql/bin/mysql --defaults-extra-file=$TMPFILE --protocol=tcp -e "SHOW SLAVE STATUS\G" > test.txt
 grep "Seconds_Behind_Master" test.txt | awk -F": " {' print "second_behind_master " $2 '}
 grep "Slave_IO_State" test.txt | awk -F": " {' print "slave_io_state " $2 '}
 grep "Slave_IO_Running" test.txt | awk -F": " {' print "slave_io_running " $2 '}
@@ -18,3 +28,4 @@ grep "Last_SQL_Errno" test.txt | awk -F": " {' print "last_sql_errno " $2 '}
 grep "Last_SQL_Error" test.txt | awk -F": " {'if ($2=="") print "last_sql_error NONE"; else print "last_sql_error " $2 ;'}
 
 rm -f test.txt
+rm -f $TMPFILE
